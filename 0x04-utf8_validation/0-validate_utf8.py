6be17daa71f9c3128cc251ef1e54801cd00e74a4
Eslam <114030950+EslamElnajdy@users.noreply.github.com>
2024-05-26 21:44:48 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,17 +10,17 @@ def validUTF8(data):
     data: params[]
     return: boolean
     """
-     # Number of bytes to process in the current UTF-8 character
+    # Number of bytes to process in the current UTF-8 character
     num_bytes = 0
-    
+
     # Masks for checking the leading bits
     mask1 = 1 << 7
     mask2 = 1 << 6
-    
+
     for num in data:
         # Get the binary representation of the integer
         byte = num & 0xFF
-        
+
         if num_bytes == 0:
             # Determine how many bytes are in this UTF-8 character
             if (byte & mask1) == 0:
@@ -41,7 +41,7 @@ def validUTF8(data):
             # Check if it's a valid continuation byte
             if (byte & (mask1 | mask2)) != mask1:
                 return False
-        
+
         num_bytes -= 1
-    
+
     return num_bytes == 0
