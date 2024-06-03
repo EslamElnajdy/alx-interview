dd04fd9497c8b04f938540d3f8b2f324090771b5
EslamElnajdy <eslammhamed425@gmail.com>
2024-06-03 17:59:10 +0300

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -59,8 +59,9 @@ if __name__ == '__main__':
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    n = int(argv[1])
-    if type(n) is not int:
+    try:
+        n = int(argv[1])
+    except ValueError:
         print("N must be a number")
         exit(1)
     if n < 4:
