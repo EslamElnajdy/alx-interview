7506bbfd6472a4ada2018b9de79a39410010fbe5
EslamElnajdy <eslammhamed425@gmail.com>
2024-06-17 17:13:11 +0300

    initial commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+  rotate_2d_matrix - function that rotates a 2D matrix 90deg clockwise
+  matrix: a 2D list representing the matrix to rotate
+"""
+
+def rotate_2d_matrix(matrix):
+  n = len(matrix)
+  new_arr = []
+  for i in range(n):
+    temp = []
+    for j in range(n - 1, -1, -1):
+      temp.append(matrix[j][i])
+    new_arr.append(temp)
+  for i in range(n):
+    for j in range(n):
+      matrix[i][j] = new_arr[i][j]
+    
