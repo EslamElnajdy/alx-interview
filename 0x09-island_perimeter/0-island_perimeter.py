b1b9cc30cc0c54d001cd52404a641c7055cf0795
EslamElnajdy <eslammhamed425@gmail.com>
2024-07-01 01:46:26 +0300

    fix the codestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,34 +3,35 @@
 0-island
 """
 
+
 def island_perimeter(grid):
-  """
-  fun
-  """
-  n = len(grid)
-  m = len(grid[0])
-  perimeter = 0
-  for i in range(n):
-    for j in range(m):
-      if grid[i][j] == 1:
-        if i != 0:
-          if grid[i - 1][j] == 0:
-            perimeter += 1
-        else:
-          perimeter += 1
-        if j != 0:
-          if grid[i][j - 1] == 0:
-            perimeter += 1
-        else:
-          perimeter += 1
-        if i != n-1:
-          if grid[i+1][j] == 0:
-            perimeter += 1
-        else:
-          perimeter += 1
-        if j != m-1:
-          if grid[i][j+1] == 0:
-            perimeter += 1
-        else:
-          perimeter += 1
-  return perimeter
+    """
+    fun
+    """
+    n = len(grid)
+    m = len(grid[0])
+    perimeter = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                if i != 0:
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+                if j != 0:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+                if i != n-1:
+                    if grid[i+1][j] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+                if j != m-1:
+                    if grid[i][j+1] == 0:
+                        perimeter += 1
+                else:
+                    perimeter += 1
+    return perimeter
