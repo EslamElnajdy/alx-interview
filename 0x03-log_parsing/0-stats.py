9d67c53d38a125942afdb5dd6d6325d0fa174e81
EslamElnajdy <eslammhamed425@gmail.com>
2024-07-29 18:26:37 +0300

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
+
 import sys
-import signal
+import re
 
-# Initialize counters and accumulators
-file_size_total = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
-def print_statistics():
-    global file_size_total, status_counts
-    print(f"File size: {file_size_total}")
-    for status_code in sorted(status_counts.keys()):
-        if status_counts[status_code] > 0:
-            print(f"{status_code}: {status_counts[status_code]}")
-    print()
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def handle_interrupt(signal, frame):
-    print_statistics()
-    sys.exit(0)
 
-# Set up signal handler for keyboard interruption
-signal.signal(signal.SIGINT, handle_interrupt)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        
-        # Check if the line matches the expected format
-        parts = line.split()
-        if len(parts) == 7:
-            try:
-                status_code = int(parts[5])
-                file_size = int(parts[6])
-                
-                # Check if the status code is in the expected set
-                if status_code in status_counts:
-                    file_size_total += file_size
-                    status_counts[status_code] += 1
-            except ValueError:
-                continue
-        
-        # Print statistics every 10 lines
-        if line_count % 10 == 0:
-            print_statistics()
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-except KeyboardInterrupt:
-    print_statistics()
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                log["file_size"] += file_size
+
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
